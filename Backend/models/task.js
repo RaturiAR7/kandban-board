042c332f0e42da8c4ef590eea47a8c3011bbd39c
Anshul <anshulraturi007@gmail.com>
2025-04-05 16:42:07 +0530

    Connected to db config and other basic config

---
+++ Backend/models/task.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "done"],
+      default: "todo",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", TaskSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -2,21 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const connectDB = require("./config/database");
 
 const app = express();
 const PORT = 5000;
+
+
+///MongoDB connection
+connectDB();
 
 ////Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-///MongoDB connection
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/kanban", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
